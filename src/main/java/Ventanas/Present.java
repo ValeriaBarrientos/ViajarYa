14074640682d5005c4ad606357253d811150ce53
ValeriaBarrientos <valeriab2630@gmail.com>
2018-07-15 12:44:57 -0300

    Ventana Present

--- src/main/java/Ventanas/Present.java
+++ src/main/java/Ventanas/Present.java
@@ -102,7 +102,7 @@ public class Present extends FrameManager{
         gRes1.insets = new Insets(0,0,10,0);
         panel1.add(labelMail,gRes1);
        
-        textMail = new JTextField("valeriab@gmail.com");
+        textMail = new JTextField("lilianag@gmail.com");
         textMail.setColumns(15);
         gRes1.gridx = 1;
         gRes1.gridwidth = GridBagConstraints.REMAINDER;
@@ -119,7 +119,7 @@ public class Present extends FrameManager{
         gRes1.insets = new Insets(0,0,10,0);
         panel1.add(labelPass,gRes1);
         
-        textPass = new JPasswordField("vale123");
+        textPass = new JPasswordField("lili123");
         textPass.setColumns(15);
         gRes1.gridx = 1;
         gRes1.gridwidth =GridBagConstraints.REMAINDER;
